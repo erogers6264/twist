23c146306224d0de3e929dcfaec2d7804bac112d
Ethan Rogers <erogers6264@gmail.com>
2017-09-08 15:30:21 -0700

    Click on X to delete Todo

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -2,3 +2,9 @@
 $("li").click(function () {
     $(this).toggleClass("completed");
 });
+
+// Click on X to delete Todo
+$("span").click(function(event){
+    $(this).parent().fadeOut();
+    event.stopPropagation();
+});
