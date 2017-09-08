c8974f84f5b241058fca54d7c563e9b70a01a62f
Ethan Rogers <erogers6264@gmail.com>
2017-09-08 15:08:45 -0700

    Refactor completed task styling

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,7 +1,4 @@
 // Check Off Specific Todos By Clicking
 $("li").click(function () {
-    $(this).css({
-        color: "#E0E0E0",
-        textDecoration: "line-through"
-    })
+    $(this).toggleClass("completed");
 });
